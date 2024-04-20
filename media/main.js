7c92af0553756766bb251cdbf37cc620bfc7100e
Murashige <hiroki.mura23@gmail.com>
2024-04-20 16:44:15 +0900

    Added the ability to save links and notes.

--- media/main.js
+++ media/main.js
@@ -1,9 +1,11 @@
 (function () {
     const vscode = acquireVsCodeApi();
+    const modal = document.getElementById('modal');
     var tmpText;
+    var tmpLink;
+    var tmpMemo;
+    var isOpenModal = false;
 
-    document.querySelector(".hexamind-hexagon.drag").draggable = true;
-    document.querySelector(".hexamind-hexagon.drag").addEventListener("dragstart", onDragStart);
     document.querySelectorAll(".hexamind-hexagon.drop").forEach((element) => {
         element.addEventListener("drop", onDrop);
         element.addEventListener("dragover", onDragover);
@@ -19,28 +21,42 @@
 
     function onDragStart(event) {
         event.dataTransfer.setData("text", event.currentTarget.id);
-        tmpText = event.currentTarget.firstElementChild.firstElementChild.value;
+        tmpText = document.getElementById(event.currentTarget.id + "-text").value;
+        tmpLink = document.getElementById(event.currentTarget.id + "-link").value;
+        tmpMemo = document.getElementById(event.currentTarget.id + "-memo").value;
     }
 
     function onDrop(event) {
         event.currentTarget.classList.remove("dragging");
         var targetId = event.dataTransfer.getData("text");
-        var element = document.getElementById(targetId);
-        element.firstElementChild.firstElementChild.value = event.currentTarget.firstElementChild.firstElementChild.value;
-        event.currentTarget.firstElementChild.firstElementChild.value = tmpText;
+        var txtElem = changeData(targetId + "-text", event.currentTarget.id + "-text", tmpText);
+        var linkElem = changeData(targetId + "-link", event.currentTarget.id + "-link", tmpLink);
+        var memoElem = changeData(targetId + "-memo", event.currentTarget.id + "-memo", tmpMemo);
 		vscode.postMessage({
 			type: 'set',
             data: [
                 {
                     id: event.currentTarget.id,
-                    text: event.currentTarget.firstElementChild.firstElementChild.value
+                    text: txtElem[1].value,
+                    link: linkElem[1].value,
+                    memo: memoElem[1].value,
                 },
                 {
                     id: targetId,
-                    text: element.firstElementChild.firstElementChild.value
+                    text: txtElem[0].value,
+                    link: linkElem[0].value,
+                    memo: memoElem[0].value,
                 }
             ]
 		});
+    }
+
+    function changeData(srcId, dstId, tmpText){
+        var srcElem = document.getElementById(srcId);
+        var dstElem =  document.getElementById(dstId);
+        srcElem.value = dstElem.value;
+        dstElem.value = tmpText;
+        return [srcElem, dstElem];
     }
 
     function onDragenter(event) {
@@ -53,6 +69,10 @@
 
     function onDragover(event) {
         event.preventDefault();
+    }
+
+    function onFocusout(event){
+        postData(event.currentTarget.parentNode.parentNode.id);
     }
 
     function onMouseEnter(event) {
@@ -80,9 +100,16 @@
         });
     }
 
-    function onFocusout(event){
+    function postData(id){
 		vscode.postMessage({
-			type: 'set', data:[{id: event.currentTarget.parentNode.parentNode.id, text: event.currentTarget.value}]
+			type: 'set', data:[
+                {
+                    id: id,
+                    text: document.getElementById(id + "-text").value,
+                    link: document.getElementById(id + "-link").value,
+                    memo: document.getElementById(id + "-memo").value,
+                }
+            ]
 		});
     }
 
@@ -95,13 +122,22 @@
 			json = JSON.parse(text);
             json.data.forEach(element => {
                 try {
-                    document.getElementById(element.id).firstElementChild.firstElementChild.value = element.text;
+                    document.getElementById(element.id + "-text").value = cleanValue(element.text);
+                    document.getElementById(element.id + "-link").value = cleanValue(element.link);
+                    document.getElementById(element.id + "-memo").value = cleanValue(element.memo);
                 } catch (error) {
                 }
             });
         } catch {
             console.log('Failed to load data.');
         }
+    }
+
+    function cleanValue(value){
+        if (value == undefined || value == "undefined") {
+            value = "";
+        }
+        return value;
     }
 
 	window.addEventListener('message', event => {
@@ -119,4 +155,58 @@
 	if (state) {
 		updateContent(state.text);
 	}
+
+
+    document.querySelectorAll(".hexamind-hexagon a.link").forEach((element) => {
+        element.addEventListener("click", onLinkClick);
+    });
+
+    document.querySelectorAll(".hexamind-hexagon a.memo").forEach((element) => {
+        element.addEventListener("click", onMemoClick);
+    });
+
+    function onLinkClick(event) {
+        event.preventDefault();
+        modal.style.display = "block";
+        isOpenModal = true;
+        const linkValueId = event.currentTarget.parentNode.parentNode.id + "-link";
+        document.getElementById("modal-type").value = "link";
+        document.getElementById("modal-id").value = event.currentTarget.parentNode.parentNode.id;
+        document.getElementById("modal-target-id").value = linkValueId;
+        document.getElementById("modal-value").value = document.getElementById(linkValueId).value;
+    }
+
+    function onMemoClick(event) {
+        event.preventDefault();
+        modal.style.display = "block";
+        isOpenModal = true;
+        const memoValueId = event.currentTarget.parentNode.parentNode.id + "-memo";
+        document.getElementById("modal-type").value = "memo";
+        document.getElementById("modal-id").value = event.currentTarget.parentNode.parentNode.id;
+        document.getElementById("modal-target-id").value = memoValueId;
+        document.getElementById("modal-value").value = document.getElementById(memoValueId).value;
+    }
+
+    document.querySelector(".modal > .modal-wrapper > .modal-head > button.close").addEventListener("click", onCloseModal);
+    document.querySelector(".modal").addEventListener("click", onCloseModal);
+    document.querySelector(".modal > .modal-wrapper > .modal-body > textarea").addEventListener("change", onChangeModal);
+
+    function onCloseModal(event) {
+        if (isOpenModal && event.target.nodeName != "TEXTAREA") {
+            modal.style.display = "none";
+            isOpenModal = false;
+        }
+    }
+
+    function onChangeModal(event) {
+        if (isOpenModal && event.target.nodeName == "TEXTAREA") {
+            const id = document.getElementById("modal-id").value;
+            const targetId = document.getElementById("modal-target-id").value;
+            const changeValue = document.getElementById("modal-value").value;
+            var targetElem = document.getElementById(targetId);
+            targetElem.value = changeValue;
+            postData(id);
+        }
+    }
+
 }());
